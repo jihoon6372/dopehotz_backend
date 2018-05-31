87e202a218d15c5018f2083cd769a657a58b145b
jihoon <jihoon6372@hanmail.net>
2018-05-31 22:42:06 +0900

    트랙 댓글 url 추가

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TrackViewSet
+from .views import TrackViewSet, TrackCommentViewSet
 
 app_name = 'track'
 
 urlpatterns = [
     path('', TrackViewSet.as_view({'get':'list', 'post': 'create'})),
-    path('<int:track_id>/', TrackViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}))
+    path('<int:track_id>/', TrackViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
+    path('<int:track>/comments/', TrackCommentViewSet.as_view({'get':'list'}))
 ]
