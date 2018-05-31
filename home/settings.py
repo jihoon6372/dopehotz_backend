9649a19c14810fa94d00ae3cbebb5c481f07a34f
jihoon6372 <jihoon6372@hanmail.net>
2018-05-31 13:22:29 +0900

    secret 문구 추가

--- home/settings.py
+++ home/settings.py
@@ -20,6 +20,7 @@ from .settings_secret import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Application definition
