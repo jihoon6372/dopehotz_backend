757979160705d9ab13b9c0fde77c3fcc11a325f0
jihoon6372 <jihoon6372@hanmail.net>
2018-05-31 18:55:42 +0900

    대댓글 시리얼라이저 추가

--- tracks/serializers.py
+++ tracks/serializers.py
@@ -1,12 +1,49 @@
 from rest_framework import serializers
-from .models import Track
+from .models import Track, TrackComment
 
-# from accounts.models import Profile, User
 from accounts.serializers import UserSerializer 
+
+
+class CommentChildSerializer(serializers.ModelSerializer):
+	user = UserSerializer(read_only=True)
+	class Meta:
+		model = TrackComment
+		fields =(
+			'id',
+			'user',
+			'content',
+			'created_at'
+		)
+
+class CommentSerializer(serializers.ModelSerializer):
+	user = UserSerializer(read_only=True)	
+	children = CommentChildSerializer(read_only=True, many=True)
+	created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+
+	class Meta:
+		model = TrackComment
+		fields = (
+			'id',
+			'user',
+			'children',
+			'content',
+			'created_at'
+		)
+
+		read_only_fields = (
+			'user',
+			'created_at'
+		)
+
+
+
+
 
 class TrackSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    comment = CommentSerializer(read_only=True, many=True)
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+
 
     class Meta:
         model = Track
@@ -27,8 +64,9 @@ class TrackSerializer(serializers.ModelSerializer):
 			'likes',
 			'track_score',
 			'on_stage',
+			'comment',
             'created_at'
-            )
+        )
 
         read_only_fields = (
             'user',
@@ -42,4 +80,5 @@ class TrackSerializer(serializers.ModelSerializer):
 			'download_url',
 			'waveform_url',
             'created_at'
-            )
+        )
+
