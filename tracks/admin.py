7b718ca9026d27f791db5ee4ed7321080bec8851
jihoon6372 <jihoon6372@hanmail.net>
2018-05-31 13:23:04 +0900

    어드민 트랙 리스트페이지 필드 추가

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Track
 
-admin.site.register(Track)
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'track_id')
+
+admin.site.register(Track, TrackAdmin)
