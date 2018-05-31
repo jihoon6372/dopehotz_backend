d5cf23b4a5628ecd9eddd9b7c4f56a9fad71b50d
jihoon <jihoon6372@hanmail.net>
2018-06-01 00:45:15 +0900

    댓글 url POST 추가

--- tracks/urls.py
+++ tracks/urls.py
@@ -7,5 +7,5 @@ app_name = 'track'
 urlpatterns = [
     path('', TrackViewSet.as_view({'get':'list', 'post': 'create'})),
     path('<int:track_id>/', TrackViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
-    path('<int:track>/comments/', TrackCommentViewSet.as_view({'get':'list'}))
+    path('<int:track>/comments/', TrackCommentViewSet.as_view({'get':'list', 'post': 'create'}))
 ]
