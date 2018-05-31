bc7fe54c3e8278949d1c8317c8a8d9d7f92d583a
jihoon6372 <jihoon6372@hanmail.net>
2018-05-31 13:54:15 +0900

    트랙 댓글 어드민사이트 등록

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Track
+from .models import Track, TrackComment
 
 
 class TrackAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'track_id')
 
+
+class TrackCommentAdmin(admin.ModelAdmin):
+    list_display = ('track', 'user', 'content')
+
 admin.site.register(Track, TrackAdmin)
+admin.site.register(TrackComment, TrackCommentAdmin)
+
