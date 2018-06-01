9046a8b2274a0214efaf60736a631f6a752e9c77
jihoon <jihoon6372@hanmail.net>
2018-06-02 00:18:20 +0900

    유저 삭제 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import UserViewSet
 app_name = 'accounts'
 
 urlpatterns = [
-    path('<int:pk>/', UserViewSet.as_view({'get':'retrieve', 'put':'update'})),
+    path('<int:pk>/', UserViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path('me/', UserViewSet.as_view({'get':'me'}))
 ]
