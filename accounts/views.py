e2dbffbfa29d41ebe0f722a9f400240d73450bf0
jihoon6372 <jihoon6372@hanmail.net>
2018-06-01 18:49:23 +0900

    유저 뷰셋 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework import serializers, viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
+from django.contrib.auth import get_user_model
+
+from .models import Profile
+from .serializers import UserSerializer, ProfileSerializer
+from home.permissions import IsOwnerOrReadOnly
 
 def login_cancelled(request):
     return redirect('/')
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    queryset = get_user_model().objects.prefetch_related('profile').all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    @list_route()
+    def me(self, request, *args, **kwargs):
+        User = get_user_model()
+        self.object = get_object_or_404(User, pk=request.user.id)
+        serializer = self.get_serializer(self.object)
+        return Response(serializer.data)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('profile', False)
+        print('2')
+        print(partial)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    
+
