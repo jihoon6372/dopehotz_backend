393d82572932ed2e103378326ffc0ab475933a23
jihoon6372 <jihoon6372@hanmail.net>
2018-06-01 10:21:38 +0900

    트랙 댓글 수정 및 삭제 추가

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import TrackViewSet, TrackCommentViewSet
+from .views import TrackViewSet, TrackCommentViewSet, TrackCommentDetailViewSet
 
 app_name = 'track'
 
 urlpatterns = [
     path('', TrackViewSet.as_view({'get':'list', 'post': 'create'})),
     path('<int:track_id>/', TrackViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
-    path('<int:track>/comments/', TrackCommentViewSet.as_view({'get':'list', 'post': 'create'}))
+    path('<int:track>/comments/', TrackCommentViewSet.as_view({'get':'list', 'post': 'create'})),
+    path('<int:track>/comments/<int:pk>/', TrackCommentDetailViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
 ]
