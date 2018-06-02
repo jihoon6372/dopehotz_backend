75d24cfb329b7572621b4d520e7f6dfb0a2ebf18
jihoon <jihoon6372@hanmail.net>
2018-06-02 18:37:26 +0900

    profile 시리얼라이저 읽기전용 필드 추가 및 user 시리얼라이저 업데이트시 profile 데이터 수정되도록 적용

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,12 +4,18 @@ from accounts.models import Profile, User
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
+
         fields = (
             'soundcloud_url',
             'profile_picture',
             'greeting',
             'clips_greeting',
             'likes_greeting'
+        )
+
+        read_only_fields = (
+            'soundcloud_url',
+            'profile_picture'
         )
 
 
@@ -24,3 +30,19 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'profile'
         )
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', False)
+        profile = instance.profile
+
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        if profile_data:
+            profile.greeting = profile_data.get('greeting', profile.greeting)
+            profile.clips_greeting = profile_data.get('clips_greeting', profile.clips_greeting)
+            profile.likes_greeting = profile_data.get('likes_greeting', profile.likes_greeting)
+            profile.save()
+
+        return instance
