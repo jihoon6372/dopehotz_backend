82c47955a36b9ef57cdeeeb872e696fee8cfd180
jihoon <jihoon6372@hanmail.net>
2018-06-03 02:59:57 +0900

    플레이리스트 그룹 뷰셋 추가

--- playlists/views.py
+++ playlists/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import PlayListGroupSerializer
+from .models import PlayListGroup, PlayList
+
+class PlayListGroupViewSet(viewsets.ModelViewSet):
+    serializer_class = PlayListGroupSerializer
+    queryset = PlayListGroup.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            user=request.user,
+        )
+
+        return Response(serializer.data)
+
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().filter(user=request.user))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
