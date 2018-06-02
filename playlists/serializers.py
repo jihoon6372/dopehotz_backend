eafb4246f1880d7022efcbfd44df37967d35036d
jihoon <jihoon6372@hanmail.net>
2018-06-03 03:19:06 +0900

    플레이리스트 그룹 디테일 시리얼라이저 및 플레이리스트 시리얼라이저 추가

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -2,6 +2,19 @@ from home.serializers import TimeSetSerializer
 
 from .models import PlayList, PlayListGroup
 
+
+# 플레이리스트 시리얼라이저
+class PlayListSerializer(TimeSetSerializer):
+    class Meta:
+        model = PlayList
+
+        fields = (
+            'id',
+            'track',
+            'order',
+            'created_at'
+        )
+
 # 플레이리스트 그룹 시리얼라이저
 class PlayListGroupSerializer(TimeSetSerializer):
     class Meta:
@@ -16,4 +29,20 @@ class PlayListGroupSerializer(TimeSetSerializer):
 
         read_only_fields = (
             'user',
+        )
+
+
+# 플레이리스트 그룹 디테일 시리얼라이저
+class PlayListGroupDetailSerializer(PlayListGroupSerializer):
+    # play_list = PlayListSerializer()
+
+    class Meta:
+        model = PlayListGroup
+
+        fields = (
+            'id',
+            'user',
+            'name',
+            'play_list',
+            'created_at'
         )
