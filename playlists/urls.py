ecd3258f4187b4423a2751e512246cfe3aa1d566
jihoon <jihoon6372@hanmail.net>
2018-06-05 00:44:13 +0900

    플레이리스트 url 뷰셋 변경

--- playlists/urls.py
+++ playlists/urls.py
@@ -7,6 +7,6 @@ app_name = 'track'
 urlpatterns = [
     path('group/', PlayListGroupViewSet.as_view({'get':'list', 'post': 'create'})),
     path('group/<int:pk>/', PlayListGroupViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
-    path('group/<int:pk>/change/', PlayListUpdateViewSet.as_view({'put':'update'})),
+    path('group/<int:pk>/change/', PlayListGroupViewSet.as_view({'put':'update'})),
     path('test/', test),
 ]
