09f5971033ace52f461b75ad0621630b318f0051
jihoon <jihoon6372@hanmail.net>
2018-06-07 22:47:36 +0900

    플레이리스트 트랙 추가 url 추가

--- playlists/urls.py
+++ playlists/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('group/', PlayListGroupViewSet.as_view({'get':'list', 'post': 'create'})),
     path('group/<int:pk>/', PlayListGroupViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path('group/<int:pk>/change/', PlayListViewSet.as_view({'put':'update'})),
+    path('group/<int:pk>/add/', PlayListViewSet.as_view({'post':'create'})),
 ]
