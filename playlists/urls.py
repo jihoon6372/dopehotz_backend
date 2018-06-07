cc79113ec4953ee8851cb2fe9c7ea64be5b7845f
jihoon6372 <jihoon6372@hanmail.net>
2018-06-07 15:35:32 +0900

    플레이리스트 url 정리

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import PlayListGroupViewSet, test, PlayListUpdateViewSet
+from .views import PlayListGroupViewSet, PlayListViewSet
 
 app_name = 'track'
 
 urlpatterns = [
     path('group/', PlayListGroupViewSet.as_view({'get':'list', 'post': 'create'})),
     path('group/<int:pk>/', PlayListGroupViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
-    path('group/<int:pk>/change/', PlayListGroupViewSet.as_view({'put':'update'})),
-    path('test/', test),
+    path('group/<int:pk>/change/', PlayListViewSet.as_view({'put':'update'})),
 ]
