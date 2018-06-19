c48129380ab0c6ec5f9233dcf208823505b18380
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-06-19 21:29:47 +0900

    트랙 뷰셋 알고리즘 변경 (사운드클라우드, 유튜브 따로 처리용)

--- tracks/views.py
+++ tracks/views.py
@@ -25,46 +25,25 @@ class TrackViewSet(viewsets.ModelViewSet):
     queryset = Track.objects.prefetch_related(Prefetch('comment', queryset=TrackComment.objects.filter(parent=None)), 'comment__user__profile', 'comment__children__user__profile', 'like').select_related('user__profile').filter(is_deleted=False)
 
     def create(self, request, *args, **kwargs):
-        # 사우드클라우드 계정인지 확인
-        if not request.user.profile.soundcloud_id:
-            return Response({'message' : '사운드클라우드 계정으로 로그인 후 이용해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+        track_type = request.data.get('type', '')
         
-        # 트랙 입력 체크
-        if 'track_id' not in request.data:
-            return Response({'track_id' : ['이 필드는 필수 항목입니다.']}, status=status.HTTP_400_BAD_REQUEST)        
+        if not track_type:
+            return Response({'track_type' : ['이 필드는 필수 항목입니다.']}, status=status.HTTP_400_BAD_REQUEST)        
+            
+        # 트랙 타입이 사우드클라우드일 경우 처리
+        elif track_type == 'soundcloud':
+            data = self.soundcloud(request, args, kwargs)
+            
+        # 트랙 타입이 유튜브일 경우 처리()
+        elif track_type == 'youtube':
+            raise InvalidAPIQuery('지원하지 않는 타입입니다.')
+            
+        else:
+            raise InvalidAPIQuery('지원하지 않는 타입입니다.')
 
-        # 사운드클라우드 트랙 데이터 가져오기
-        # sc_data = requests.get('http://api.soundcloud.com/tracks/'+request.data['track_id']+'?client_id='+settings.SOCIAL_AUTH_SOUNDCLOUD_KEY).json()
-        sc_data = soundcloud_track_data(request.data['track_id'])
-
-        # 사운드클라우드의 게시물이 존재하는지 체크
-        if 'errors' in sc_data:
-            return Response({'message' : '사운드클라우드의 게시물을 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # 사운드클라우드의 트랙이 게시자의 트랙게시물인지 체크
-        if sc_data['user']['id'] != request.user.profile.soundcloud_id:
-            return Response({'message' : '사운드클라우드의 본인 트랙 게시물만 등록 가능합니다.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # 기타 저장용 데이터 가져오기
-        genre = sc_data.get('genre', '')
-        image_url = sc_data.get('artwork_url', '')
-        download_url = sc_data.get('download_url', '')
-        waveform_url = sc_data.get('waveform_url', '')
-        duration = sc_data.get('duration', '')
-
-        # 저장
-        serializer = TrackSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            user=request.user,
-            genre=genre,
-            image_url=image_url,
-            download_url = download_url,
-            waveform_url = waveform_url,
-            duration = duration
-        )
-
-        return Response(serializer.data)
+        
+        return Response(data)
+        
 
     def update(self, request, *args, **kwargs):
         post_id = request.data.get('track_id', '')
@@ -105,6 +84,49 @@ class TrackViewSet(viewsets.ModelViewSet):
         #     return TrackSerializer_v2
 
         return self.serializer_class
+
+    def soundcloud(self, request, *args, **kwargs):
+        # 사우드클라우드 계정인지 확인
+        if not request.user.profile.soundcloud_id:
+            return Response({'message' : '사운드클라우드 계정으로 로그인 후 이용해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 트랙 입력 체크
+        if 'track_id' not in request.data:
+            return Response({'track_id' : ['이 필드는 필수 항목입니다.']}, status=status.HTTP_400_BAD_REQUEST)        
+
+        # 사운드클라우드 트랙 데이터 가져오기
+        # sc_data = requests.get('http://api.soundcloud.com/tracks/'+request.data['track_id']+'?client_id='+settings.SOCIAL_AUTH_SOUNDCLOUD_KEY).json()
+        sc_data = soundcloud_track_data(request.data['track_id'])
+
+        # 사운드클라우드의 게시물이 존재하는지 체크
+        if 'errors' in sc_data:
+            return Response({'message' : '사운드클라우드의 게시물을 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 사운드클라우드의 트랙이 게시자의 트랙게시물인지 체크
+        # if sc_data['user']['id'] != request.user.profile.soundcloud_id:
+        #     return Response({'message' : '사운드클라우드의 본인 트랙 게시물만 등록 가능합니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 기타 저장용 데이터 가져오기
+        genre = sc_data.get('genre', '')
+        image_url = sc_data.get('artwork_url', '')
+        download_url = sc_data.get('download_url', '')
+        waveform_url = sc_data.get('waveform_url', '')
+        duration = sc_data.get('duration', '')
+
+        # 저장
+        serializer = TrackSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            user=request.user,
+            genre=genre,
+            image_url=image_url,
+            download_url = download_url,
+            waveform_url = waveform_url,
+            duration = duration
+        )
+
+        # return Response(serializer.data)
+        return serializer.data
 
 
 # 트랙 댓글 버전관리 상속용 뷰셋
