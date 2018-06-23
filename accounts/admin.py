7a7ae6dce0b2fae3c1de7f5e69f3124ebf275ad1
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-06-23 13:17:27 +0900

    프로필 어드민 리스트 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('user', 'name', 'soundcloud_id')
+
+    def name(self, obj):
+        return obj.user.last_name+obj.user.first_name
+    name.short_description = '이름'
+
+admin.site.register(Profile, ProfileAdmin)
