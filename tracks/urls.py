8207031594ac913d38398022915228c07b6a0a44
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-07-03 22:28:48 +0900

    트랙 좋아요 리스트 url 추가

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TrackViewSet, TrackCommentViewSet, TrackCommentDetailViewSet, TrackLikeViewSet
+from .views import TrackViewSet, TrackCommentViewSet, TrackCommentDetailViewSet, TrackLikeViewSet, TrackLikeListViewSet
 
 app_name = 'track'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:track>/comments/<int:pk>/', TrackCommentDetailViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path('<int:track>/like/', TrackLikeViewSet.as_view({'post':'create'})),
     path('on-stage/', TrackViewSet.as_view({'get':'on_stage'})),
+    path('likes/list/', TrackLikeListViewSet.as_view({'get':'list'})),
 ]
