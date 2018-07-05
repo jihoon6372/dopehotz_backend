2a4b30c5b32a69436c6ac9449172f38aa0a633c1
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-07-05 20:55:41 +0900

    tower 호스트 추가

--- home/hosts.py
+++ home/hosts.py
@@ -5,4 +5,5 @@ host_patterns = patterns('',
     host(r'api', settings.ROOT_URLCONF, name='api'),
     host(r'adm', settings.ADMIN_URLCONF, name='admin'),
     host(r'auth', settings.AUTH_URLCONF, name='auth'),
+    host(r'tower', settings.TOWER_URLCONT, name='tower'),
 )
