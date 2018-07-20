d758c78db5c03a05758c27288fd816e12d9a514c
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-07-21 00:54:59 +0900

    tower urls 추가

--- home/urls_tower.py
+++ home/urls_tower.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from django.conf import settings
 
-from .views_tower import index
+from .views_tower import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('mytracks/', mytracks),
+    path('post/', post),
+    path('profile/', profile),
+    path('dashboard/', dashboard),
+    path('post/select/', post_select),
+    path('post/new/', post_new),
 ]
 
 if settings.DEBUG:
