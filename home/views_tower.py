ca14effea34c0e826e3c7dae1385b916496afe2b
jihoonLee <ijihoon@jihoonLeeui-MacBook-Pro.local>
2018-07-21 00:56:11 +0900

    tower 뷰 함수 임시용 추가

--- home/views_tower.py
+++ home/views_tower.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tower/home.html', {})
+
+
+def mytracks(request):
+    return render(request, 'tower/mytracks.html', {})
+
+
+def post(request):
+    return render(request, 'tower/post.html', {})
+
+def profile(request):
+    return render(request, 'tower/profile.html', {})
+
+def dashboard(request):
+    return render(request, 'tower/dashboard.html', {})
+
+def post_select(request):
+    return render(request, 'tower/select.html', {})
+
+def post_new(request):
+    return render(request, 'tower/new.html', {})
