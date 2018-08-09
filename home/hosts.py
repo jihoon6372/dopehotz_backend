ab808814d11eba88d69a2cb49cd5a90d21957fe6
jihoonLee <jihoon6372@hanmail.net>
2018-08-10 00:05:20 +0900

    host 리스트 추가

--- home/hosts.py
+++ home/hosts.py
@@ -6,4 +6,14 @@ host_patterns = patterns('',
     host(r'adm', settings.ADMIN_URLCONF, name='admin'),
     host(r'auth', settings.AUTH_URLCONF, name='auth'),
     host(r'tower', settings.TOWER_URLCONT, name='tower'),
+
+    host(r'test.api', settings.ROOT_URLCONF, name='test-api'),
+    host(r'test.adm', settings.ADMIN_URLCONF, name='test-admin'),
+    host(r'test.auth', settings.AUTH_URLCONF, name='test-auth'),
+    host(r'test.tower', settings.TOWER_URLCONT, name='test-tower'),
+
+    host(r'local.api', settings.ROOT_URLCONF, name='local-api'),
+    host(r'local.adm', settings.ADMIN_URLCONF, name='local-admin'),
+    host(r'local.auth', settings.AUTH_URLCONF, name='local-auth'),
+    host(r'local.tower', settings.TOWER_URLCONT, name='local-tower'),
 )
