d106926a12c4ddc8939829da406a26f99c4ce30c
jihoonLee <jihoon6372@hanmail.net>
2018-08-12 15:42:05 +0900

    host url 추가

--- home/hosts.py
+++ home/hosts.py
@@ -7,11 +7,6 @@ host_patterns = patterns('',
     host(r'auth', settings.AUTH_URLCONF, name='auth'),
     host(r'tower', settings.TOWER_URLCONT, name='tower'),
 
-    host(r'test.api', settings.ROOT_URLCONF, name='test-api'),
-    host(r'test.adm', settings.ADMIN_URLCONF, name='test-admin'),
-    host(r'test.auth', settings.AUTH_URLCONF, name='test-auth'),
-    host(r'test.tower', settings.TOWER_URLCONT, name='test-tower'),
-
     host(r'local.api', settings.ROOT_URLCONF, name='local-api'),
     host(r'local.adm', settings.ADMIN_URLCONF, name='local-admin'),
     host(r'local.auth', settings.AUTH_URLCONF, name='local-auth'),
