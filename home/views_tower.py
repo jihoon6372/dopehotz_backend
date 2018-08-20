4255f9c5548b636a5914e004282edec839df16f9
jihoonLee <jihoon6372@hanmail.net>
2018-08-20 21:47:53 +0900

    profile post 처리 알고리즘 수정

--- home/views_tower.py
+++ home/views_tower.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from accounts.forms import ProfileForm
 
 def index(request):
     return render(request, 'tower/home.html', {})
@@ -12,7 +14,26 @@ def post(request):
     return render(request, 'tower/post.html', {})
 
 def profile(request):
-    return render(request, 'tower/profile.html', {})
+    if request.method == "POST":
+        form = ProfileForm(request.POST)        
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            
+            profile = request.user.profile
+            profile.likes_greeting = form.cleaned_data['likes_greeting']
+            profile.clips_greeting = form.cleaned_data['clips_greeting']
+            profile.nickname = form.cleaned_data['nickname']
+            profile.greeting = form.cleaned_data['greeting']
+            profile.location = form.cleaned_data['location']
+            profile.crew = form.cleaned_data['crew']
+
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'tower/profile.html', {'form': form})
+
 
 def dashboard(request):
     return render(request, 'tower/dashboard.html', {})
