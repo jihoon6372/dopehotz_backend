f16ad2bd2c44d1c5c3f8825c078700c5f4bfb5ec
jihoonLee <jihoon6372@hanmail.net>
2018-08-31 22:23:14 +0900

    아답터.py 추가

---
+++ home/adapter.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import get_next_redirect_url
+
+class MyAccountAdapter(DefaultAccountAdapter):
+    def get_login_redirect_url(self, request):
+        next_url = request.session.get('next')
+        
+        if next_url:
+            path = next_url
+        else:
+            path = settings.LOGIN_REDIRECT_URL
+        
+        return path.format(username=request.user.username)
