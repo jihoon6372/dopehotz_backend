55371526c8d3ae4b142bc3008f69705d4c1dceb1
jihoonLee <jihoon6372@hanmail.net>
2018-09-07 16:56:01 +0900

    오픈마이크, 트랙(ME) url 추가

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TrackViewSet, TrackCommentViewSet, TrackCommentDetailViewSet, TrackLikeViewSet, TrackLikeListViewSet
+from .views import TrackViewSet, TrackCommentViewSet, TrackCommentDetailViewSet, TrackLikeViewSet, TrackLikeListViewSet, TrackMeViewSet
 
 app_name = 'track'
 
@@ -11,5 +11,10 @@ urlpatterns = [
     path('<int:track>/comments/<int:pk>/', TrackCommentDetailViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path('<int:track>/like/', TrackLikeViewSet.as_view({'post':'create'})),
     path('on-stage/', TrackViewSet.as_view({'get':'on_stage'})),
+    path('open-mic/', TrackViewSet.as_view({'get':'open_mic'})),
     path('likes/list/', TrackLikeListViewSet.as_view({'get':'list'})),
+    path('me/', TrackMeViewSet.as_view({'get':'me'})),
+    path('me/count/', TrackMeViewSet.as_view({'get':'get_count'})),
+    path('me/on-stage/', TrackMeViewSet.as_view({'get':'on_stage'})),
+    path('me/open-mic/', TrackMeViewSet.as_view({'get':'open_mic'})),
 ]
