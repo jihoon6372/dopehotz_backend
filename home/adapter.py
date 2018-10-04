0bd80595516e651a448fd8e9a680a3a369e4a39d
jihoonLee <jihoon6372@hanmail.net>
2018-10-04 21:46:50 +0900

    allauth adapter 넥스트url 로컬호스트, 타워쪽일 경우 토큰 붙여서 리디렉트 적용

--- home/adapter.py
+++ home/adapter.py
@@ -1,14 +1,31 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.account.utils import get_next_redirect_url
+from rest_auth.utils import jwt_encode
+from tld import get_tld
+import re
 
+    
 class MyAccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
         next_url = request.session.get('next')
         del request.session['next']
-                
+
         if next_url:
-            path = next_url
+            localhost = re.compile('localhost')
+            token = jwt_encode(request.user)
+
+            if localhost.search(next_url):
+                path = 'http://localhost:8080/#/auth/{}'.format(token)
+            else:
+                try:
+                    detail_url = get_tld(next_url, as_object=True)
+                    if detail_url.subdomain in ['tower', 'local.tower']:
+                        path = '{}://{}.{}/#/auth/{}'.format(detail_url.parsed_url.scheme, detail_url.subdomain, detail_url.fld, token)
+                    else:
+                        path = next_url
+                except:
+                    path = next_url
         else:
             path = settings.LOGIN_REDIRECT_URL
         
