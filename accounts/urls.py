3ae79cc63c465b0797788537ccf66b38fae7441a
jihoon <jihoon6372@hanmail.net>
2018-10-12 00:40:53 +0900

    유저 사운드클라우드 토큰 가져오기 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import UserViewSet
+from .views import UserViewSet, UserMeViewSet
 app_name = 'accounts'
 
 urlpatterns = [
     path('<int:pk>/', UserViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
-    path('me/', UserViewSet.as_view({'get':'me'}))
+    path('me/', UserMeViewSet.as_view({'get':'me'})),
+    path('me/soundcloud-token/', UserMeViewSet.as_view({'get': 'get_soundcloud_token'}))
 ]
