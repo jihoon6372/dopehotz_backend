a85ce0e3f5df972951aecb1ac7722dae36add739
jihoon <jihoon6372@hanmail.net>
2018-10-23 01:37:18 +0900

    공지사항, 신고하기 뷰셋 추가

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import NoticeSerializer, ReportSerializer
+from .models import Notice, Report
+from home.permissions import IsOwnerOrReadOnly
+
+class NoticeViewSet(viewsets.ModelViewSet):
+    lookup_field = 'slug'
+    serializer_class = NoticeSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
+    queryset = Notice.objects.filter(is_delete=False)
+
+
+class ReportViewSet(viewsets.ModelViewSet):
+    serializer_class = ReportSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+    queryset = Report.objects
+
+    def create(self, request, *args, **kwargs):
+
+        # 저장
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            user=request.user
+        )
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
