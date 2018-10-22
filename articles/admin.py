973db9d49fbbde34c10bf6dba2885f32682173a6
jihoon <jihoon6372@hanmail.net>
2018-10-23 01:47:24 +0900

    신고하기, 의견보내기 어드민 등록

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Notice
+from .models import Notice, Report
 
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created_at', 'updated_at')
 
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'created_at')
+
+
 admin.site.register(Notice, NoticeAdmin)
+admin.site.register(Report, ReportAdmin)
