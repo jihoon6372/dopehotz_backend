f041fdb67b44a125d8c0ffda6528e09384007b4d
jihoon <jihoon6372@hanmail.net>
2018-10-24 12:47:55 +0900

    Report 타입 어드민 등록

--- articles/admin.py
+++ articles/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import Notice, Report
+from .models import Notice, Report, ReportType
 
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created_at', 'updated_at')
 
 
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'created_at')
+    list_display = ('user', 'content', 'report_type', 'created_at')
+
+
+class ReportTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
 
 
 admin.site.register(Notice, NoticeAdmin)
 admin.site.register(Report, ReportAdmin)
+admin.site.register(ReportType, ReportTypeAdmin)
