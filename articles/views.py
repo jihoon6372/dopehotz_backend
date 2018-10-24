6dd68d13e5397666792efc9b3043a0f713e3925c
jihoon <jihoon6372@hanmail.net>
2018-10-24 12:49:41 +0900

    Report Viewset 타입 및 검증항목 추가

--- articles/views.py
+++ articles/views.py
@@ -18,12 +18,21 @@ class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects
 
     def create(self, request, *args, **kwargs):
+        # 타입 입력 체크
+        if 'report_type' not in request.data:
+            return Response({'report_type' : ['이 필드는 필수 항목입니다.']}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            report_type = int(request.data.get('report_type'))
+        except:
+            return Response({'report_type' : ['지원되지 않는 타입입니다.']}, status=status.HTTP_400_BAD_REQUEST)
 
         # 저장
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(
-            user=request.user
+            user=request.user,
+            report_type_id=report_type
         )
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
